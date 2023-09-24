6b9787f311cb0872c54dd775653c5e40bf2a1bb6
Felipemr <felipe.mon.rod@gmail.com>
2023-09-24 20:56:36 -0300

    add de img

---
+++ app-files/data.js
@@ -0,0 +1,237 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada-educao-infantil",
+      "name": "Entrada Educação Infantil",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 0.09124369167304103,
+        "pitch": 0.41191048196166946,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4000430363682614,
+          "pitch": 0.5975024248329142,
+          "rotation": 0.7853981633974483,
+          "target": "1-sala-educao-infantil-v1"
+        },
+        {
+          "yaw": 1.395852435691845,
+          "pitch": 0.7933976153179962,
+          "rotation": 0.7853981633974483,
+          "target": "2-sala-educao-infantil-v2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sala-educao-infantil-v1",
+      "name": "Sala Educação Infantil V1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": -0.06879697198798418,
+        "pitch": -0.2797622655021641,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5128447523294977,
+          "pitch": -0.8003945122371174,
+          "rotation": 1.5707963267948966,
+          "target": "0-entrada-educao-infantil"
+        },
+        {
+          "yaw": -0.5149279507078948,
+          "pitch": -0.6140215386954804,
+          "rotation": 4.71238898038469,
+          "target": "2-sala-educao-infantil-v2"
+        },
+        {
+          "yaw": -0.519332237196318,
+          "pitch": -0.4201756995919652,
+          "rotation": 4.71238898038469,
+          "target": "3-playground-infantil"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sala-educao-infantil-v2",
+      "name": "Sala Educação Infantil V2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": -0.6171493075393002,
+        "pitch": -0.24077899899776156,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9972787776976304,
+          "pitch": -0.6710432924725627,
+          "rotation": 7.853981633974483,
+          "target": "0-entrada-educao-infantil"
+        },
+        {
+          "yaw": -0.9887619257294205,
+          "pitch": -0.4823009558446927,
+          "rotation": 7.853981633974483,
+          "target": "1-sala-educao-infantil-v1"
+        },
+        {
+          "yaw": -0.9766487028027129,
+          "pitch": -0.3030038238858772,
+          "rotation": 4.71238898038469,
+          "target": "3-playground-infantil"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-playground-infantil",
+      "name": "Playground infantil",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 0.0020234403525840605,
+        "pitch": -0.39212579836777195,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0468303595163366,
+          "pitch": -0.0958990577934653,
+          "rotation": 4.71238898038469,
+          "target": "1-sala-educao-infantil-v1"
+        },
+        {
+          "yaw": -3.0289058275609655,
+          "pitch": 0.15595589810868304,
+          "rotation": 4.71238898038469,
+          "target": "2-sala-educao-infantil-v2"
+        },
+        {
+          "yaw": -3.0591847839106663,
+          "pitch": -0.34554376051032065,
+          "rotation": 4.71238898038469,
+          "target": "0-entrada-educao-infantil"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Colégio Futura Educação Infantil 360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
